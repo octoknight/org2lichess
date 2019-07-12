66f4a18ca5c705fb57555ee94cf125471fbc7e52
ProgramFOX <programfox@hotmail.be>
2019-07-12 16:13:43 +0200

    Route a simple page to / using Rocket

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,23 @@
+#![feature(proc_macro_hygiene, decl_macro)]
+
+#[macro_use] extern crate rocket;
+extern crate rocket_contrib;
+
+use rocket_contrib::templates::Template;
+use std::collections::HashMap;
+
+fn empty_context() -> HashMap<u8, u8> {
+    HashMap::new()
+}
+
+#[get("/")]
+fn index() -> Template {
+    Template::render("index", &empty_context())
+}
+
 fn main() {
-    println!("Hello, world!");
+    rocket::ignite()
+        .attach(Template::fairing())
+        .mount("/", routes![index])
+        .launch();
 }
