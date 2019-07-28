6d08561f61ef143bc4721c1f77b4f2cdd6c296fe
ProgramFOX <programfox@hotmail.be>
2019-07-28 16:41:28 +0200

    Remove unused struct

--- src/config.rs
+++ src/config.rs
@@ -1,21 +1,6 @@
 use serde::Deserialize;
 
 #[derive(Deserialize)]
-pub struct ConfigOld {
-    pub url: String,
-    pub lichess: String,
-    pub client_id: String,
-    pub client_secret: String,
-    pub connection_string: String,
-    pub azolve_api: String,
-    pub azolve_api_pwd: String,
-    pub team_id: String,
-    pub lichess_admin_id: String,
-    pub personal_api_token: String,
-    pub expiry_check_interval_seconds: u64,
-}
-
-#[derive(Deserialize)]
 pub struct Config {
     pub server: ServerConfig,
     pub lichess: LichessConfig,
@@ -26,7 +11,7 @@ pub struct Config {
 pub struct ServerConfig {
     pub url: String,
     pub expiry_check_interval_seconds: u64,
-    pub db_connection_string: String
+    pub db_connection_string: String,
 }
 
 #[derive(Deserialize)]
