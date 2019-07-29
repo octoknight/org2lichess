8c5d56beca1a96a457d19ad8103fbc73585fd069
ProgramFOX <programfox@hotmail.be>
2019-07-29 18:39:43 +0200

    Better Azolve API request implementation

--- src/azolve.rs
+++ src/azolve.rs
@@ -8,14 +8,22 @@ pub fn verify_user(
     azolve_url: &str,
     azolve_password: &str,
 ) -> Result<bool, ErrorBox> {
-    let req = Request::new(
-        Method::GET,
-        Url::parse(&format!("{}?userId=AzolveAPI&password={}&clientReference=ECF&objectName=Cus_SSO&objectType=sp&meters=MID|{};{}",
-        azolve_url,
-        azolve_password,
-        member_id,
-        member_password))?,
-    );
+    let mut url = Url::parse(azolve_url)?;
+    {
+        let mut query = url.query_pairs_mut();
+        query.append_pair("userId", "AzolveAPI");
+        query.append_pair("password", &azolve_password);
+        query.append_pair("clientReference", "ECF");
+        query.append_pair("objectName", "Cus_SSO");
+        query.append_pair("objectType", "sp");
+        query.append_pair(
+            "parameters",
+            &format!("MID|Me{};Password|{}", member_id, member_password),
+        );
+    }
+    println!("{}", url);
+    let req = Request::new(Method::GET, url);
     let response = http_client.execute(req)?.text()?;
-    Ok(response == "{Verification successful}")
+    println!("{}", &response);
+    Ok(response.trim() == "[[\"Return Code\",\"Message\"],[\"1\",\"Success\"]]")
 }
