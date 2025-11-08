55fd37ac3fe439772ddaf433a47867fb967f1c11
Thomas Daniels <daniels.thomas@pm.me>
2025-11-08 12:16:46 +0100

    Update other dependencies

--- src/randstr.rs
+++ src/randstr.rs
@@ -1,10 +1,10 @@
 use rand::Rng;
 
 pub fn random_string() -> Result<String, std::str::Utf8Error> {
-    let mut rng = rand::thread_rng();
+    let mut rng = rand::rng();
     let mut oauth_state_bytes: [u8; 64] = [0; 64];
     for x in &mut oauth_state_bytes {
-        *x = (rng.gen::<u8>() % 26) + 97;
+        *x = (rng.random::<u8>() % 26) + 97;
     }
     Ok(std::str::from_utf8(&oauth_state_bytes)?.to_string())
 }
