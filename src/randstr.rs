8cf6a492225680193177863882c9abd5cd32a4f0
Thomas Daniels <daniels.thomas@pm.me>
2026-04-24 23:57:11 +0200

    Fix random_string

--- src/randstr.rs
+++ src/randstr.rs
@@ -4,5 +4,8 @@ pub fn random_string() -> Result<String, std::str::Utf8Error> {
     let mut rng = rand::rng();
     let mut oauth_state_bytes: [u8; 64] = [0; 64];
     rng.fill_bytes(&mut oauth_state_bytes);
+    for i in 0..oauth_state_bytes.len() {
+        oauth_state_bytes[i] = (oauth_state_bytes[i] % 26) + 97;
+    }
     Ok(std::str::from_utf8(&oauth_state_bytes)?.to_string())
 }
